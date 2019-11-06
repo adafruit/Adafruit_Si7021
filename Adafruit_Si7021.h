6be32c87608d8e2327bf7776f45797a8ea707f7f
ChuckVanzant <cvv@austin.rr.com>
2019-11-06 12:15:48 -0600

    Add support for sensor heater
    Added support for enabling/disabling/checking sensor heater.
    Updated example INO file to toggle the heater enabled state every 30 seconds.

--- Adafruit_Si7021.h
+++ Adafruit_Si7021.h
@@ -40,6 +40,7 @@
 #define SI7021_READRHT_REG_CMD           0xE7 /**< Read RH/T User Register 1 */
 #define SI7021_WRITEHEATER_REG_CMD       0x51 /**< Write Heater Control Register */
 #define SI7021_READHEATER_REG_CMD        0x11 /**< Read Heater Control Register */
+#define SI7021_REG_HTRE_BIT              0x02 /**< Control Register Heater Bit */
 #define SI7021_ID1_CMD                   0xFA0F /**< Read Electronic ID 1st Byte */
 #define SI7021_ID2_CMD                   0xFCC9 /**< Read Electronic ID 2nd Byte */
 #define SI7021_FIRMVERS_CMD              0x84B8 /**< Read Firmware Revision */
@@ -69,6 +70,8 @@ class Adafruit_Si7021 {
   void reset();
   void readSerialNumber();
   float readHumidity();
+  void heater(bool h);
+  bool isHeaterEnabled();
 
   /*!
   *  @brief  Returns sensor revision established during init 
